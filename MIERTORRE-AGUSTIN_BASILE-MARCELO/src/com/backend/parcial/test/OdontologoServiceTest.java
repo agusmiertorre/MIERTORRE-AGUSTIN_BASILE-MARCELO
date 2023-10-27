49f02e9700007e79958fa4539c285b7842a64e91
Agustin <agustinmiertorre@gmail.com>
2023-10-26 23:02:33 -0300

    Finalizando test pendiente 3

--- MIERTORRE-AGUSTIN_BASILE-MARCELO/src/com/backend/parcial/test/OdontologoServiceTest.java
+++ MIERTORRE-AGUSTIN_BASILE-MARCELO/src/com/backend/parcial/test/OdontologoServiceTest.java
@@ -1,7 +1,34 @@
 package com.backend.parcial.test;
 
+import com.backend.parcial.dao.impl.OdontologoDaoH2;
+import com.backend.parcial.model.Odontologo;
+import com.backend.parcial.service.OdontologoService;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OdontologoServiceTest {
+    private OdontologoService odontologoService = new OdontologoService(new OdontologoDaoH2());
 
+
+    @BeforeAll
+    static void doBefore() {
+        Connection connection = null;
+        try {
+            Class.forName("org.h2.Driver");
+            connection = DriverManager.getConnection("jdbc:h2:~/c1parcial;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
